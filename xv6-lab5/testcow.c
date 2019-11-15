33163cdde0535f255be40238055cc30e94f2757e
Ole-jeger Hoffstuen <oj@pop-os.localdomain>
2019-11-14 17:44:07 -0700

    added commentary for the lab work as well as uncommented the print statements in testcow.c

--- xv6-lab5/testcow.c
+++ xv6-lab5/testcow.c
@@ -7,18 +7,18 @@ int main(){
         int pid = 0;
 
         if ((pid = fork()) == 0){
-                // printf(1, "Number of free pages before changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
+                printf(1, "Number of free pages before changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
                 i++;
-                // printf(1, "Number of free pages after changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
+                printf(1, "Number of free pages after changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
         } else {
                 if ((pid = fork()) == 0){
-                        // printf(1, "Number of free pages before changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
+                        printf(1, "Number of free pages before changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
                         i++;
-                        // printf(1, "Number of free pages after changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
+                        printf(1, "Number of free pages after changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
                 } else {
-                        // printf(1, "Number of free pages before changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
+                        printf(1, "Number of free pages before changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
                         i++;
-                        // printf(1, "Number of free pages after changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
+                        printf(1, "Number of free pages after changing a variable process with pid: %d : %d\n", getpid(), getNumFreePages());
 
                         while(wait() != -1)
                                 continue;
